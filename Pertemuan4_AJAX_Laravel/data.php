8dc7ca0dbc5a9dcc210749b400c7ffc9cfa0fe36
AfrizalDwiNugraha136 <afrizalnugroho0304@gmail.com>
2026-04-24 00:09:19 +0700

    update

--- Pertemuan4_AJAX_Laravel/data.php
+++ Pertemuan4_AJAX_Laravel/data.php
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-<?php
-header('Content-Type: application/json');
-
-$data = [
-    'nama' => 'Afrizal Dwi Nugraha',
-    'pekerjaan' => 'Mahasiswa Informatika / Web Developer',
-    'lokasi' => 'Purwokerto'
-];
-echo json_encode($data);
-=======
 <?php
 header('Content-Type: application/json');
 
@@ -19,5 +8,4 @@ $data = [
 ];
 
 echo json_encode($data);
->>>>>>> f3ae24f7c065a8738a14839a64c4fb0f4662e530
 ?>
